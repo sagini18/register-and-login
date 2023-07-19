ac5ef659608534ed443d2640f8441b93080425f6
sagini18 <saginisaju@gmail.com>
2023-07-20 04:03:18 +0530

    bug fixed

--- server/index.js
+++ server/index.js
@@ -26,9 +26,9 @@ app.use(
     key: "userId",
     secret: "my secret",
     resave: false,
-    saveUninitialized: false,
+    unInitialized: false,
     cookie: {
-      expires: 60 * 60 * 24, //session will expired after 24 hours
+      expires: 1000 * 60 * 5, //session will expire after 5 minutes
     },
   })
 );
@@ -48,6 +48,12 @@ db.connect((err) => {
     "CREATE TABLE users(id INT AUTO_INCREMENT PRIMARY KEY,username varchar(23),password varchar(500))";
   db.query(users, () => console.log("Users table is created"));
 });
+app.post("/set-cookie", (req, res) => {
+  const username = req.body?.username;
+  // set cookie, cookie is available for 15 minutes and it is accessible only by server via http request
+  res.cookie("username", username, { maxAge: 900000, httpOnly: true });
+  res.send("Cookie set!");
+});
 
 app.post("/register", (req, res) => {
   const username = req.body.username;
@@ -66,9 +72,11 @@ app.post("/register", (req, res) => {
     );
   });
 });
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+app.get("/login/:username/:password", (req, res) => {
+  const username = req.params.username;
+  const password = req.params.password;
+  // retrieve cookie
+  console.log("username from cookie:", req?.cookies?.username);
 
   // const sql = "SELECT * FROM users WHERE username = ? AND password = ?";
   const sql = "SELECT * FROM users WHERE username = ?;";
@@ -81,11 +89,13 @@ app.post("/login", (req, res) => {
         if (response) {
           const id = result[0].id;
           const username = result[0].username;
-          const token = jwt.sign({ id, username }, "jwtSecretIsMySecret", {
-            expiresIn: 300,
-          });
+          // generate token
+          const payload = { id, username };
+          const jwtSecret = "jwtSecretIsMySecret";
+          const token = jwt.sign(payload, jwtSecret, { expiresIn: 300 });
+
           req.session.user = result;
-          res.json({ auth: true, token });
+          res.json({ auth: true, token, sessionViews: req.session.views });
         } else {
           res.json({
             auth: false,
@@ -98,16 +108,26 @@ app.post("/login", (req, res) => {
     }
   });
 });
-app.get("/login", (req, res) => {
+app.get("/loggedUser", (req, res) => {
+  // express-session
+  if (req.session.views) {
+    req.session.views++;
+  } else {
+    req.session.views = 1;
+  }
   if (req.session.user) {
-    res.json({ loggedIn: true, user: req.session.user });
+    res.json({
+      loggedIn: true,
+      user: req.session.user,
+      sessionViews: req.session.views,
+    });
   } else {
     res.json({ loggedIn: false });
   }
 });
 // middleware
 const verifyJWT = (req, res, next) => {
-  const token = req.headers["x-access-token"];
+  const token = req.headers["x-access-token12"];
   if (!token) {
     res.send("We need token ");
   } else {
@@ -115,7 +135,8 @@ const verifyJWT = (req, res, next) => {
       if (err) {
         res.json({ auth: false, message: "You failed to authenticate" });
       } else {
-        req.userId = decoded.id;
+        console.log("decoded: ", decoded);
+        req.userIdToken = decoded.id;
         next();
       }
     });
@@ -125,7 +146,14 @@ const verifyJWT = (req, res, next) => {
 app.get("/isUserAuth", verifyJWT, (req, res) => {
   res.send("you are authenticated");
 });
-
+app.get("/logout", (req, res) => {
+  req.session.views = 0;
+  req.session.destroy();
+  console.log(
+    "is cookies alive after deleting session: ",
+    req.cookies.username
+  );
+});
 app.listen(3001, () => {
   console.log("server is running");
 });
